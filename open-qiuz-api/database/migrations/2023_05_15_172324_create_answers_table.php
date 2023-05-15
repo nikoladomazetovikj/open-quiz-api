a1e312cea40a8d0a783a7a687b8f46d18e266d10
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-15 19:26:53 +0200

    feat: database created

---
+++ open-qiuz-api/database/migrations/2023_05_15_172324_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('answer_1');
+            $table->string('answer_2');
+            $table->string('answer_3');
+            $table->string('answer_4');
+            $table->boolean('answer_1_is_true');
+            $table->boolean('answer_2_is_true');
+            $table->boolean('answer_3_is_true');
+            $table->boolean('answer_4_is_true');
+            $table->unsignedBigInteger('question_id');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
