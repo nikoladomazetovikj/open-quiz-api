00f1abdae99259b2cb9c73c5aa52d0e6a3bd74a2
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-19 00:11:14 +0200

    fix: home component title

--- open-quiz-api-frontend/src/components/home/Home.js
+++ open-quiz-api-frontend/src/components/home/Home.js
@@ -1,3 +1,17 @@
-const Home = props => {};
+import {Col, Container, Row} from "react-bootstrap";
+
+const Home = props => {
+
+    return (
+        <Container>
+            <Row className="justify-content-center">
+                <Col xs="auto" className="my-5">
+                    <h1>Welcome To Open API Quiz</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+
+};
 
 export default Home;
--- open-quiz-api-frontend/src/pages/index.js
+++ open-quiz-api-frontend/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "@/components/layouts/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "@/components/home/Home";
 
 function App() {
     return (
         <>
             <Header/>
+            <Home/>
         </>
     )
 }
