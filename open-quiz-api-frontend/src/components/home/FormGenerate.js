aff9812dbfc7004d65aec0a882f4efc570a194aa
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-19 09:58:31 +0200

    fix: card form button in the home component

--- open-quiz-api-frontend/src/components/home/FormGenerate.js
+++ open-quiz-api-frontend/src/components/home/FormGenerate.js
@@ -1,15 +1,15 @@
-import {Card, Col, Form, Row} from "react-bootstrap";
+import {Button, Card, Col, Form, Row} from "react-bootstrap";
 
 const FormGenerate  = () => {
     return (
         <Row className="justify-content-center">
             <Col xs="auto" className="my-3">
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '28rem' }}>
                     <Card.Body>
                         <Card.Title>Generate API</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Please select from the form items</Card.Subtitle>
                         <Form>
-                            <Form.Group controlId="formCategory">
+                            <Form.Group controlId="formCategory" className="mt-3">
                                 <Form.Label>Select Category:</Form.Label>
                                 <Form.Control as="select">
                                     <option>All</option>
@@ -17,7 +17,7 @@ const FormGenerate  = () => {
                                 </Form.Control>
                             </Form.Group>
 
-                            <Form.Group controlId="formNumQuestions">
+                            <Form.Group controlId="formNumQuestions" className="mt-3">
                                 <Form.Label>Select Number of Questions:</Form.Label>
                                 <Form.Control as="select">
                                     <option>5</option>
@@ -27,7 +27,7 @@ const FormGenerate  = () => {
                                 </Form.Control>
                             </Form.Group>
 
-                            <Form.Group controlId="formAnyCategory">
+                            <Form.Group controlId="formAnyCategory" className="mt-3">
                                 <Form.Label>Select Category:</Form.Label>
                                 <Form.Control as="select">
                                     <option>Any</option>
@@ -35,7 +35,7 @@ const FormGenerate  = () => {
                                 </Form.Control>
                             </Form.Group>
 
-                            <Form.Group controlId="formDifficulty">
+                            <Form.Group controlId="formDifficulty" className="mt-3">
                                 <Form.Label>Select Difficulty:</Form.Label>
                                 <Form.Control as="select">
                                     <option>Easy</option>
@@ -43,8 +43,10 @@ const FormGenerate  = () => {
                                     <option>Hard</option>
                                 </Form.Control>
                             </Form.Group>
+                            <Button variant="primary" type="submit" className="mt-3">
+                                Generate Link
+                            </Button>
                         </Form>
-
                     </Card.Body>
                 </Card>
             </Col>
