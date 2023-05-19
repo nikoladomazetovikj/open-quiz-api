d6b999a47a94531107d98f52e3cd1602facbdf8e
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-19 09:42:17 +0200

    feat: enums

---
+++ open-qiuz-api/app/Enums/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum Category : int
+{
+    case SPORT = 1;
+    case HISTORY = 2;
+
+    case POLITICS = 3;
+
+    case ANIMALS = 4;
+
+    case MUSIC = 5;
+
+}
---
+++ open-qiuz-api/app/Enums/Difficulty.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum Difficulty : int
+{
+    case EASY = 1;
+    case MEDIUM = 2;
+    case HARD = 3;
+
+}
