418602b4c8fa4bbadc82d575305feaf5bf9c3172
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-19 16:48:36 +0200

    fix: width fixed

--- open-quiz-api-frontend/src/pages/contribute.js
+++ open-quiz-api-frontend/src/pages/contribute.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Header from "@/components/layouts/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddQuestion from "@/components/contribute/AddQuestion";
-import {Col, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 const Contribute = () => {
     return (
         <>
             <Header/>
-            <Row className="justify-content-center">
-                <Col xs="auto" className="my-5">
-                    <AddQuestion/>
-                </Col>
-            </Row>
+           <Container>
+               <Row className="justify-content-center">
+                   <Col xs="auto" className="my-5">
+                       <AddQuestion/>
+                   </Col>
+               </Row>
+           </Container>
         </>
     )
 };
