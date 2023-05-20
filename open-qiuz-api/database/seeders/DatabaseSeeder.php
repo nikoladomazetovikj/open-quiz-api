45e8a850bd54cb5dfbb2a64c36df3577bd958890
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-20 13:46:53 +0200

    feat: admin seeder

--- open-qiuz-api/database/seeders/DatabaseSeeder.php
+++ open-qiuz-api/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
-            DifficultySeeder::class
+            DifficultySeeder::class,
+            AdminSeeder::class
         ]);
     }
 }
