a12ce3b16f2774b05c2ff9418d301832b39c27c8
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-20 14:43:16 +0200

    fix: header rendering for admin

--- open-quiz-api-frontend/src/components/layouts/Header.js
+++ open-quiz-api-frontend/src/components/layouts/Header.js
@@ -1,7 +1,10 @@
-import React from "React";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Cookies from "js-cookie";
 
-const Header = props => {
+const Header = () => {
+    const token = Cookies.get("token");
+
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
@@ -9,11 +12,18 @@ const Header = props => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/contribute">Contribute</Nav.Link>
+                        {!token && <Nav.Link href="/">Home</Nav.Link>}
+                        {!token && <Nav.Link href="/contribute">Contribute</Nav.Link>}
                     </Nav>
                     <Nav>
-                        <Nav.Link href="/login" className="me-2">Login</Nav.Link>
+                        {token ? (
+                            <>
+                                <Nav.Link href="/dashboard" className="me-2">Dashboard</Nav.Link>
+                                <Nav.Link href="/logout" className="me-2">Logout</Nav.Link>
+                            </>
+                        ) : (
+                            <Nav.Link href="/login" className="me-2">Login</Nav.Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
