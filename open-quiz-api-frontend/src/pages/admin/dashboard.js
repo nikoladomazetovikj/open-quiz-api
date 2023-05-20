ced6add73770f253f85dbcd42cba3b4084926a34
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-20 15:00:28 +0200

    feat: redirect not authorized

--- open-quiz-api-frontend/src/pages/admin/dashboard.js
+++ open-quiz-api-frontend/src/pages/admin/dashboard.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Col, Container, Row} from "react-bootstrap";
 import AdminHeader from "@/components/layouts/AdminHeader";
+import {useRouter} from "next/router";
+import Cookies from "js-cookie";
 
 const Dashboard = () => {
+    const router = useRouter();
+    const token = Cookies.get("token");
+
+    useEffect(() => {
+        // Redirect to /login if token is not present
+        if (!token) {
+            router.replace("/login");
+        }
+    }, [token, router]);
     return (
         <>
             <AdminHeader/>
