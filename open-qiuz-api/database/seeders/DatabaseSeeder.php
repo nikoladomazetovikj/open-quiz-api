1cc98fe6e86a6a5c33ff7e1942d3e7db320c211b
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-22 18:35:34 +0200

    feat: sports question seeder

--- open-qiuz-api/database/seeders/DatabaseSeeder.php
+++ open-qiuz-api/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             DifficultySeeder::class,
-            AdminSeeder::class
+            AdminSeeder::class,
+            SportQuestionsSeeder::class
         ]);
     }
 }
